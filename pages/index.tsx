ab82d8178b8ea8c183eed8606f9d80474fc0a1ee
Diego Villalobos <d_villalobos@outlook.com>
2021-06-15 22:05:28 -0600

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,8 +46,8 @@ export default function Home({
             key={product.id}
             product={product}
             imgProps={{
-              width: i === 0 ? 1080 : 540,
-              height: i === 0 ? 1080 : 540,
+              width: i === 0 ? 1080 : 545,
+              height: i === 0 ? 1080 : 545,
             }}
           />
         ))}
